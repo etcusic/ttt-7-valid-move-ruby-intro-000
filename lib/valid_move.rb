ceeb28f7a32018e6f5a6e9bcb8bd9893c73553ad
Eli Cusic <tntitans31@gmail.com>
2020-07-22 15:56:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if (index >= 0 && index <= 8)
     return true
   elsif (position_taken?(board, index) == false)
-    return return true
+    return true
   else
     return false
   end
