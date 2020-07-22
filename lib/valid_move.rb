51ebbbba7275e7272441fdd961bb4626cd07e5ad
Eli Cusic <tntitans31@gmail.com>
2020-07-22 16:00:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,6 +13,8 @@ end
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return false
+  elsif(board[index] == "X" || board[index] == "O")
+    return false
   else
     return true
   end
