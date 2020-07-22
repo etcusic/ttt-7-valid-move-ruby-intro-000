d6783bbbd4427cde77e28720691713ad3ecdfe41
Eli Cusic <tntitans31@gmail.com>
2020-07-22 16:13:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if (index >= 0 && index <= 8)
     return true
-  elsif (position_taken?(board, index) == false)
+  elsif (position_taken?(board, index) == true)
     return true
   else
     return false
@@ -12,10 +12,10 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
-    return false
-  elsif(board[index] == "X" || board[index] == "O")
     return true
+  # elsif(board[index] == "X" || board[index] == "O")
+  #   return true
   else
-    return true
+    return false
   end
 end
